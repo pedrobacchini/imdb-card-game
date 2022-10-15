51af25ad0142fd7c20cb8dfe82c9cfb3d494b560
pedrobacchini <pedrobacchini@outlook.com>
2022-10-15 19:09:58 -0300

    create project

---
+++ src/main/java/com/pedrobacchini/imdbcardgame/ImdbCardGameApplication.java
@@ -0,0 +1,13 @@
+package com.pedrobacchini.imdbcardgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbCardGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbCardGameApplication.class, args);
+	}
+
+}
